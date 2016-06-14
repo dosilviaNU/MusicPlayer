4e2879541d7d3b568fac77a3c0e8b18b509cf780
Jake Dec <dec.j@husky.neu.edu>
2016-06-14 14:20:22 -0400

    Updated MidiView,p currently working unthreaded # Conflicts: #	.idea/workspace.xml

--- src/cs3500/music/view/MidiView.java
+++ src/cs3500/music/view/MidiView.java
@@ -1,7 +1,6 @@
 package cs3500.music.view;
 
 import java.util.Collection;
-import java.util.List;
 
 import javax.sound.midi.Instrument;
 import javax.sound.midi.MidiChannel;
@@ -10,7 +9,6 @@ import javax.sound.midi.MidiUnavailableException;
 import javax.sound.midi.Synthesizer;
 
 import cs3500.music.model.MidiNote;
-import cs3500.music.model.MidiSheet;
 import cs3500.music.model.MidiComposition;
 
 /**
@@ -18,10 +16,24 @@ import cs3500.music.model.MidiComposition;
  */
 public class MidiView implements IMusicView<MidiComposition>, Runnable {
   MidiComposition comp;
+  Synthesizer synth;
+  MidiChannel[] mc;
+  Instrument[] instr;
 
-  MidiView(MidiComposition composition) {
+
+  public MidiView(MidiComposition composition) {
     this.comp = composition;
+    try {
+      synth = MidiSystem.getSynthesizer();
+      synth.open();
+      mc = synth.getChannels();
+      instr = synth.getDefaultSoundbank().getInstruments();
+    } catch (MidiUnavailableException e1) {
+      e1.printStackTrace();
+    }
   }
+
+
   @Override
   public MidiComposition display() {
     return null;
@@ -29,7 +41,45 @@ public class MidiView implements IMusicView<MidiComposition>, Runnable {
 
   @Override
   public void run() {
+    this.setInstruments();
+    this.playComp();
+  }
 
+  private void setInstruments() {
+    for (int i = 0; i < 16; i++) {
+      mc[i].programChange(0, comp.getInstrument(i));
+    }
+  }
+
+  public void play(int channel, int note, int volume, boolean play) throws
+          InterruptedException {
+    if (play) {
+      mc[channel].noteOn(note, volume);
+    } else {
+      mc[channel].noteOff(note, volume);
+    }
+
+  }
+
+  public void playComp() {
+    int delay = this.comp.getTempo();
+    try {
+      for (int beat = 0; beat < comp.size(); beat++) {
+        Collection<MidiNote> notes = comp.getNotes(beat);
+        for (MidiNote n : notes) {
+          if (n.getStart() == beat) {
+            this.play(n.getChannel(), n.getValue(), n.getVolume(), true);
+          }
+        }
+        Thread.sleep(delay);
+        for (MidiNote n : notes) {
+          if (n.getStart() + n.getDuration() == beat) {
+            this.play(n.getChannel(), n.getValue(), n.getVolume(), false);
+          }
+        }
+      }
+    } catch (Exception e) {
+    }
   }
 
 
