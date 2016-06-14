a80bbabb9b84a56db33f310a8091a0d55002fd76
Jake Dec <dec.j@husky.neu.edu>
2016-06-14 10:59:08 -0400

    Merge branch 'master' of https://github.ccs.neu.edu/jdec/MusicEditor into David
    # Conflicts:
    #	.idea/workspace.xml

---
+++ src/cs3500/music/model/IComposition.java
@@ -0,0 +1,8 @@
+package cs3500.music.model;
+
+/**
+ * Interface for a multichannel audio file file
+ * Created by Jake on 6/14/2016.
+ */
+public interface IComposition {
+}
---
+++ src/cs3500/music/model/MidiComposition.java
@@ -0,0 +1,8 @@
+package cs3500.music.model;
+
+/**
+ * Interface for a composition of multiple midi sheets.
+ * Created by Jake on 6/14/2016.
+ */
+public class MidiComposition implements IComposition {
+}
