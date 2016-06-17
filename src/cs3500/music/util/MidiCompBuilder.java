5bab0d1bae5b13e3b5a444389cf10afef3cd4d37
Jake Dec <dec.j@husky.neu.edu>
2016-06-17 14:06:11 -0400

    Builders Updated
    # Conflicts:
    #	.idea/workspace.xml

--- src/cs3500/music/util/MidiCompBuilder.java
+++ src/cs3500/music/util/MidiCompBuilder.java
@@ -4,7 +4,9 @@ import cs3500.music.model.MidiComposition;
 import cs3500.music.model.MidiNote;
 
 /**
- * Created by Jake on 6/14/2016.
+ * Implementation for Composition build that produces MidiCompositions<MidiNotes> This
+ * implementation will throw all errors in notes or tempos that are being built with this
+ * CompositionBuilder caused by invalid data being entered. Created by Jake on 6/14/2016.
  */
 public class MidiCompBuilder implements CompositionBuilder<MidiComposition> {
   MidiComposition comp;
@@ -19,15 +21,20 @@ public class MidiCompBuilder implements CompositionBuilder<MidiComposition> {
   }
 
   @Override
-  public CompositionBuilder<MidiComposition> setTempo(int tempo) {
+  public CompositionBuilder<MidiComposition> setTempo(int tempo) throws IllegalArgumentException {
     comp.setTempo(tempo);
     return this;
   }
 
   @Override
   public CompositionBuilder<MidiComposition> addNote(int start, int end, int instrument,
-                                                     int pitch, int volume) {
-    comp.addNote(new MidiNote(pitch, start, end + 1 - start, volume, instrument - 1));
+                                                     int pitch, int volume)
+          throws IllegalArgumentException {
+    try {
+    comp.addNote(new MidiNote(pitch, start, end + 1 - start, volume, instrument - 1)); }
+    catch(Exception e) {
+      throw new IllegalArgumentException("Error in MidiCompBuilder: " + e.getMessage());
+    }
     return this;
   }
 }
