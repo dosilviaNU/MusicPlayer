2616633190c03d60b37795951fa24f921a3e20f8
dosilvia <silvia.d@husky.neu.edu>
2016-06-16 20:30:39 -0400

    Removed anything related to mouse clicks.

--- src/cs3500/music/view/GuiView/NoteDisplay.java
+++ src/cs3500/music/view/GuiView/NoteDisplay.java
@@ -6,8 +6,7 @@ import cs3500.music.model.INote;
 import javax.swing.*;
 
 import java.awt.*;
-import java.awt.event.*;
-import java.awt.event.MouseEvent;
+
 import java.util.*;
 import java.util.List;
 
@@ -27,7 +26,6 @@ public class NoteDisplay extends JComponent {
   private static final int leftBorder = GuiView.BEAT_WIDTH;
   private static int windowHeight;
   private static int windowWidth;
-  private static Map<String, List<INote>> pitchBeatMap;
   private static Collection<INote> notes;
 
   /**
@@ -45,15 +43,6 @@ public class NoteDisplay extends JComponent {
     windowWidth = (spread[2] + 1) * leftBorder;
 
     setPreferredSize(new Dimension(windowWidth, windowHeight));
-
-    //Build the pitch/beat->INote map.
-    pitchBeatMap = new HashMap<String, List<INote>>();
-    notes = sheet.getNotes();
-    //buildPitchBeatMap();
-
-    //for (INote note : notes) {
-    //  addNoteToPitchMap(note);
-    //}
    setVisible(true);
   }
 
@@ -105,10 +94,8 @@ public class NoteDisplay extends JComponent {
       }
       g.drawLine(leftBorder, i, windowWidth, i);
     }
-
   }
-
-
+  
   /**
    * Given a note value, return the y position for that note.
    *
@@ -123,34 +110,6 @@ public class NoteDisplay extends JComponent {
   }
 
   /**
-   * Maps pitch/beat combos to a list of INotes at that pitch/beat.
-   */
-  private void buildPitchBeatMap() {
-    for (int i = spread[1]; i >= spread[0]; i--) {
-      String temp = valueToPitchString(i);
-      for (int j = 0; j <= spread[2]; j++) {
-        pitchBeatMap.put(temp + j, new ArrayList<INote>());
-      }
-    }
-  }
-
-  /**
-   * Adds a note to the pitchBeatMap.
-   *
-   * @param note INote to be added.
-   */
-  private void addNoteToPitchMap(INote note) {
-    int start = note.getStart();
-    int end = start + note.getDuration();
-    int noteValue = note.getValue();
-    for (int i = start; i < end; i++) {
-      String key = valueToPitchString(noteValue) + i;
-      pitchBeatMap.get(key).add(note);
-    }
-  }
-
-
-  /**
    * Given a note value, returns its string representation.
    *
    * @param noteValue Value to convert to a string.
@@ -161,51 +120,6 @@ public class NoteDisplay extends JComponent {
     result.append(INote.Pitch.fromValue(noteValue % 12).toString());
     result.append((noteValue / 12) - 1);
     return result.toString();
-  }
-
-  /**
-   * Given a mouse clicks x/y coordinates, returns a list of all notes at that position.
-   *
-   * @param x Mouse x.
-   * @param y Mouse y.
-   * @return All notes at given x/y.
-   */
-  private static List<INote> mouseClickToNotes(int x, int y) {
-    StringBuilder key = new StringBuilder();
-
-    //Find and convert pitchValue.
-    int pitchValue = yCoordToValue(y);
-    key.append(valueToPitchString(pitchValue));
-
-    //Find and append beat value.
-    int beat = xCoordToValue(x);
-    key.append(beat);
-
-    //Use key to pull notes at given pitch/beat.
-    List<INote> tempArray = pitchBeatMap.get(key.toString());
-
-    return tempArray;
-  }
-
-  /**
-   *Given a y coordinate, return the pitch value associated with it.
-   * @param y Mouse y position.
-   * @return Pitch value associated with given y.
-   */
-  private static int yCoordToValue(int y) {
-    int ordinal = ((y - topBorder - 1) / topBorder) - 1;
-    int value = (noteMod - ordinal) + spread[0];
-    return value;
-  }
-
-  /**
-   * Given an x coordinate, return the beat number associated with it.
-   * @param x Mouse x position.
-   * @return Beat number associated with given x.
-   */
-  private static int xCoordToValue(int x) {
-    int value = ((x - leftBorder) / leftBorder);
-    return value;
   }
 
 }
