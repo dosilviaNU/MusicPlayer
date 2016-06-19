4da9cf0ab1f49587e4987b75de1fdce5fa5defe6
Jake Dec <dec.j@husky.neu.edu>
2016-06-19 14:04:28 -0400

    comments
    # Conflicts:
    #	.idea/workspace.xml

--- src/cs3500/music/view/IMusicView.java
+++ src/cs3500/music/view/IMusicView.java
@@ -1,11 +1,16 @@
 package cs3500.music.view;
 
 import javax.swing.*;
+
 import java.util.jar.JarEntry;
 
 /**
+ * Interface for different types of views for compositions
  * Created by David on 6/12/2016.
  */
 public interface IMusicView<T> {
-    public void display();
+  /**
+   * Display, or play, the view
+   */
+  public void display();
 }
